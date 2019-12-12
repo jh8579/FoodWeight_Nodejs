69574ffb7d215aa5eb88ae16a4cea4d2b4e5381d
Huh Jin-Ho <huhjin-ho@Huhui-MacBookPro.local>
2019-12-12 23:59:19 +0900

    :dd

--- deployment_scripts/start.sh
+++ deployment_scripts/start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 sudo pm2 stop node-app
 # actually start the server
 sudo pm2 start /opt/food_waste/app.js -i 0 --name "node-app"
--- deployment_scripts/stop.sh
+++ deployment_scripts/stop.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
 sudo pm2 stop node-app
 sleep 10
