09a4b44062a2e626b04142cea8add766c020ab67
Huh Jin-Ho <huhjin-ho@Huhui-MacBookPro.local>
2019-12-14 03:11:16 +0900

    df

--- deployment_scripts/deploy.sh
+++ deployment_scripts/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
 cd /opt/food_test
 sudo npm install
 sudo node /opt/food_test/src/create_database.js
-
 sudo node /opt/food_test/src/insert_db.js
