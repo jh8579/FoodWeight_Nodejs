82c017e9be22984de91e0a75439bb8160c8dffe9
Huh Jin-Ho <huhjin-ho@Huhui-MacBookPro.local>
2019-12-14 02:34:48 +0900

    dfdfs

--- deployment_scripts/deploy.sh
+++ deployment_scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /opt/food_test
 sudo npm install
-sudo node /opt/food_test/src/create_database.js
+sudo node /opt/food_testaws/src/create_database.js
+sudo node /opt/food_testaws/src/insert_db.js
